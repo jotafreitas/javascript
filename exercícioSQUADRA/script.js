2ddff54c34a373398a889ebea3831ce59db1ca6d
jotafreitas <joaocarlofreitas@hotmail.com>
2024-06-12 13:40:58 -0400

    a

---
+++ exercícioSQUADRA/script.js
@@ -0,0 +1,16 @@
+function saudacoes(){
+    window.alert("Olá, mundo!");
+}
+
+function soma() {
+    let a = 1;
+    let b = 1;
+    result = a + b;
+    window.alert(result);
+}
+
+function contador() {
+    for(i=1;i<11;i++){
+        window.alert(i);
+    }
+}
